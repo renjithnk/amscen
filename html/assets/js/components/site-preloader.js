9f682821653454d0fd5478dfa4864b3d7cdedd8a
renjithnk <renjithnks@gmail.com>
2024-06-09 22:47:30 +0530

    initial commit

--- html/assets/js/components/site-preloader.js
+++ html/assets/js/components/site-preloader.js
@@ -1,17 +1,18 @@
 /* Website Preloader: Start */
-function sitePreloader1(selector,duration){
-let preloaderElement=document.querySelector(selector);
-if(preloaderElement) {
-    document.addEventListener('DOMContentLoaded',()=>{
-        document.body.style.overflowY='hidden';
-    })
-setTimeout(function(){
-    preloaderElement.remove();  
-    document.body.removeAttribute('style'); 
-    },duration); 
+function sitePreloader1(selector, duration) {
+  document.body.style.overflow = "hidden";
+  let preloaderElement = document.querySelector(selector);
+  if (preloaderElement) {
+    document.addEventListener("DOMContentLoaded", () => {
+      document.body.style.overflowY = "hidden";
+    });
+    setTimeout(function () {
+      preloaderElement.remove();
+      document.body.removeAttribute("style");
+    }, duration);
+  }
 }
-}
-sitePreloader1('.site-preloader1',1000);
+sitePreloader1(".site-preloader1", 1000);
 /* Website Preloader: End */
 
 export default sitePreloader1;
--- html/assets/js/scripts.js
+++ html/assets/js/scripts.js
@@ -1,4 +1,4 @@
-// import sitePreloader1 from "./includes/site-preloader.js";
+import sitePreloader1 from "./components/site-preloader.js";
 import mainNavigationOne from "./components/main-navigation.js";
 import heroSlider from "./components/hero-slider.js";
 import faq from "./components/faq.js";
